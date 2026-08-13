9310e8b849eb4b71d09ba3710093806f032b3bee
lolinea <168550537+lolinea@users.noreply.github.com>
2026-08-13 17:03:19 +0700

    Add: Result View

--- Airbroad/Features/ResultView/ResultViewModel.swift
+++ Airbroad/Features/ResultView/ResultViewModel.swift
@@ -1,7 +1,26 @@
 
 import Observation
+import Foundation
 
 @Observable
 class ResultViewModel {
+    var selectedDate: Date = Date()
+    var selectedPollutant: String = "AQI"
+    
+    let pollutants: [String] = [
+        "AQI",
+        "PM2.5",
+        "PM10",
+        "O3"
+    ]
+    
+    var weekDates: [Date] {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+        
+        return (0..<5).compactMap { dayOffset in
+            calendar.date(byAdding: .day, value: dayOffset, to: today)
+        }
+    }
     
 }
--- Airbroad/Features/ResultView/Views/ResultView.swift
+++ Airbroad/Features/ResultView/Views/ResultView.swift
@@ -2,8 +2,81 @@
 import SwiftUI
 
 struct ResultView: View {
+    @Bindable var viewModel = ResultViewModel()
+    @Bindable var searchViewModel = SearchViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        ScrollView {
+            ZStack {
+                Color(.systemBackground)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    //                HStack {
+                    Text("\(viewModel.selectedDate.formatted(date: .complete, time: .omitted))")
+                        .padding(.bottom, 10)
+                        .fontWeight(.semibold)
+                    //                }
+                    
+                    
+                    HStack() {
+                        ForEach(viewModel.weekDates, id: \.self) { date in
+                            let isSelected = Calendar.current.isDate(date, inSameDayAs: viewModel.selectedDate)
+                            
+                            Button (action: {viewModel.selectedDate = date}) {
+                                VStack(spacing: 8) {
+                                    Text(date, format: .dateTime.weekday(.narrow))
+                                        .font(.callout)
+                                        .foregroundStyle(Color(.systemGray))
+                                    
+                                    Text(date, format: .dateTime.day())
+                                        .font(.title2)
+                                        .frame(width: 40, height: 40)
+                                        .foregroundStyle(isSelected ? .white : .primary)
+                                        .fontWeight(isSelected ? .semibold : .light)
+                                        .background(
+                                            Circle()
+                                                .fill(isSelected ? Color(.blue) : Color(.clear))
+                                        )
+                                }
+                            }
+                            .buttonStyle(.plain)
+                            .padding(.horizontal, 10)
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .glassEffect(.regular.tint(Color(.systemGroupedBackground)), in: .rect(cornerRadius: 20))
+                    .padding(.bottom, 15)
+                    
+                    Picker("", selection: $viewModel.selectedPollutant) {
+                        ForEach(viewModel.pollutants, id: \.self) { pollutant in
+                            Text(pollutant)
+                        }
+                        .font(.callout)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding(.bottom, 15)
+                    .padding(.horizontal, 15)
+                    
+                    PollutantPlotView(viewModel: viewModel)
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 15)
+                    
+                    DailySummaryView(viewModel: viewModel)
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 15)
+                    
+                    AboutPollutantView(viewModel: viewModel)
+                        .padding(.horizontal, 15)
+                    
+                }
+                
+                
+            }
+            
+        }
+        
     }
 }
 
